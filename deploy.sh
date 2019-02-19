66e57cf5f29b16903261f8aee429940d1f9aa58e
Dan <danphi@gmail.com>
2019-02-18 16:32:56 -0800

    remove deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-docker login registry.gitlab.com -u danphi
-docker build -t registry.gitlab.com/danphi/arenaworker .
-docker push registry.gitlab.com/danphi/arenaworker
--- java/arenaworker/src/main/java/arenaworker/abilityobjects/VortexGrenade.java
+++ java/arenaworker/src/main/java/arenaworker/abilityobjects/VortexGrenade.java
@@ -18,7 +18,7 @@ public class VortexGrenade extends AbilityObjectPhysics {
     public double damage = 10;
     public double shieldDamageMultiplier = 1;
     double vortexRadius = 250;
-    double stunDuration = 1000L;
+    long stunDuration = 1000L;
     
     public VortexGrenade(Ability ability, double rotation, double radius, double amountOfForce) {
         super(ability, ability.player.position.x, ability.player.position.y, radius, rotation, false);
@@ -65,7 +65,7 @@ public class VortexGrenade extends AbilityObjectPhysics {
                     Player p = (Player)o;
                     Vector2 diff = position.copy().subtract(o.position);
                     p.forces.add(diff.getNormalized().scale(diff.length() * 0.01));
-                    p.Freeze(stunDuration);
+                    p.Stun(stunDuration);
                 }
             }
         }
