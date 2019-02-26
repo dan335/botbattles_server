53104be639baeb8ac0df3c7d59b0a88f0407e543
Dan <danphi@gmail.com>
2019-02-25 20:27:51 -0800

    vacuum

--- java/arenaworker/src/main/java/arenaworker/abilities/Vacuum.java
+++ java/arenaworker/src/main/java/arenaworker/abilities/Vacuum.java
@@ -2,6 +2,8 @@ package arenaworker.abilities;
 
 import java.util.Set;
 
+import org.json.JSONObject;
+
 import arenaworker.Base;
 import arenaworker.Obj;
 import arenaworker.Obstacle;
@@ -11,7 +13,7 @@ import arenaworker.lib.Physics;
 public class Vacuum extends Ability {
 
     double extraForce = 5;
-    double radius = 300;
+    double radius = 250;
     long duration = 1000L;
     boolean isVacuuming = false;
     long vacuumStart;
@@ -43,6 +45,10 @@ public class Vacuum extends Ability {
 
             if (vacuumStart + duration < player.game.tickStartTime) {
                 isVacuuming = false;
+                JSONObject json = new JSONObject();
+                json.put("t", "vacuumEnd");
+                json.put("id", id);
+                player.game.SendJsonToClients(json);
             }
         }
     }
@@ -53,5 +59,11 @@ public class Vacuum extends Ability {
 
         isVacuuming = true;
         vacuumStart = player.game.tickStartTime;
+        JSONObject json = new JSONObject();
+        json.put("t", "vacuumStart");
+        json.put("shipId", player.id);
+        json.put("id", id);
+        json.put("radius", radius);
+        player.game.SendJsonToClients(json);
     }
 }
